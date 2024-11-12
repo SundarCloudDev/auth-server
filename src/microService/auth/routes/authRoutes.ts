fec066da5dfc5d0cc88abe3e55600f3bb034a662
SundarCloudDev <sundarawsdeveloper@gmail.com>
2024-11-12 10:15:04 +0530

    middleware changes

--- src/microService/auth/routes/authRoutes.ts
+++ src/microService/auth/routes/authRoutes.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from "express";
 import dotenv from "dotenv";
 import { authenticationController } from "../controller/authController";
 import { invoiceController } from "../controller/inoviceController";
-import { verifyAdmin , verifyToken} from '../../../../middleware/middleware';
+import { verifyAdmin, verifyToken } from "../../../middleware/middleware";
 dotenv.config();
 
 export const authRouter = Router();
@@ -13,7 +13,7 @@ authRouter.get("/", (req: Request, res: Response) => {
 
 authRouter.post("/login", authenticationController.loginController);
 authRouter.post("/register", authenticationController.registerController);
-authRouter.get("/getUsers",verifyToken, authenticationController.getAllUser);
+authRouter.get("/getUsers", verifyToken, authenticationController.getAllUser);
 authRouter.get(
   "/getAnalytics",
   verifyToken,
@@ -21,6 +21,14 @@ authRouter.get(
 );
 
 // invoice
-authRouter.get("/invoice",verifyAdmin, invoiceController.getAllInvoice);
-authRouter.put("/updateInvoice/:id", verifyAdmin,invoiceController.updateInvoice);
-authRouter.delete("/updateActive/:id", verifyAdmin,invoiceController.updateActive);
+authRouter.get("/invoice", verifyAdmin, invoiceController.getAllInvoice);
+authRouter.put(
+  "/updateInvoice/:id",
+  verifyAdmin,
+  invoiceController.updateInvoice
+);
+authRouter.delete(
+  "/updateActive/:id",
+  verifyAdmin,
+  invoiceController.updateActive
+);
